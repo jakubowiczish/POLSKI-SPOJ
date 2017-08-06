16dcb54fd41bffc13a06e1ea3d35cd6915da165e
jakubowiczish <jakub77plotnikowski@gmail.com>
2017-08-06 11:40:20 +0200

    dodano 2 pliki

---
+++ samolot.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned int n1, k1, n2, k2;
+    unsigned int miejsca;
+
+    cin>>n1>>k1>>n2>>k2;
+    int wynik=n1*k1+n2*k2;
+
+    cout<<wynik<<endl;
+    return 0;
+}
---
+++ suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,suma;
+    suma=0;
+    while (cin>>a)
+    {
+        suma+=a;
+        cout<<suma<<endl;
+    }
+    return 0;
+}
